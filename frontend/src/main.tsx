1a9015ab60f1e3cf42f7c31db3db30788f2b4572
Walnit <wongyueheng@gmail.com>
2024-09-21 21:26:26 +0800

    Add material-ui and router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom"
+
+import { ThemeProvider } from '@mui/material/styles'
+import { CssBaseline } from '@mui/material'
+import theme from './theme'
+
 import './index.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import App from './App';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>,
 )
